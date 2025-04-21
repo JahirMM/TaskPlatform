66d612bb40a15d3c4384d83b890de109967776a1
JahirMM <j.machuca912@gmail.com>
2025-04-21 13:07:24 -0400

    feat(Hero): implement design

---
+++ src/hero/components/Hero.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import SignInAuthButton from "@/auth/components/SignInAuthButton";
+
+import useAuthListener from "@/auth/hook/useAuthListener";
+
+function Hero() {
+  const user = useAuthListener();
+
+  return (
+    <section className="h-[calc(100vh-64px)] flex flex-col justify-center items-center px-10 mx-auto lg:container lg:px-16 xl:px-40">
+      <h1 className="flex flex-col items-center justify-center text-4xl text-center text-white sm:text-5xl sm:leading-none lg:text-7xl">
+        <span className="block">Gestiona sin esfuerzo</span>
+        <span className="block text-action">Colabora fácilmente</span>
+      </h1>
+      <p className="pt-2 my-5 text-sm text-center text-white sm:text-base lg:text-lg">
+        TaskPlatfrom es la manera más eficiente de organizar tus tareas.
+        <br className="hidden md:block" />
+        Crea espacios de trabajo, tableros y tareas en segundos. Invita a tu
+        equipo y colabora sin complicaciones.
+      </p>
+      <div className="mt-5">
+        {user ? (
+          <button
+            type="button"
+            className="inline-flex items-center px-5 py-2 mb-2 text-sm font-medium text-center text-white rounded-lg bg-action hover:bg-action-hover focus:ring-4 focus:outline-none focus:ring-action-hover me-2"
+          >
+            Mis tableros
+          </button>
+        ) : (
+          <SignInAuthButton />
+        )}
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
