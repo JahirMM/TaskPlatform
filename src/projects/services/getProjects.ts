9fe6cec4e1bebf6627897f4652e083fa4f91a9b7
JahirMM <j.machuca912@gmail.com>
2025-04-28 18:17:58 -0400

    feat(getProjects): implement ordering by most recent

--- src/projects/services/getProjects.ts
+++ src/projects/services/getProjects.ts
@@ -7,7 +7,8 @@ export const getProjects = async (): Promise<ProjectInterface[]> => {
   const { data, error } = await supabase
     .from("projects")
     .select("*")
-    .eq("owner_id", user.id);
+    .eq("owner_id", user.id)
+    .order("created_at", { ascending: false });
   if (error) throw new Error(error.message);
   if (!data) return [];
 
