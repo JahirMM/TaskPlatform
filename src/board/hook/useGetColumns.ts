584bd75892ea279245e764db0d229710186fe328
JahirMM <j.machuca912@gmail.com>
2025-07-30 18:17:14 -0400

    feat(useGetColumns): add subscription to column changes with Supabase Realtime

--- src/board/hook/useGetColumns.ts
+++ src/board/hook/useGetColumns.ts
@@ -1,7 +1,40 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getColumnsService } from "@/board/service/getColumnsService";
-import { useQuery } from "@tanstack/react-query";
+import { supabase } from "@/common/utils/supabase";
+import { useEffect } from "react";
 
 export const useGetColumns = (projectId?: string) => {
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    let channel: ReturnType<typeof supabase.channel> | undefined;
+
+    if (projectId) {
+      channel = supabase
+        .channel(`realtime:columns:${projectId}`)
+        .on(
+          "postgres_changes",
+          {
+            event: "*",
+            schema: "public",
+            table: "columns",
+          },
+          () => {
+            queryClient.invalidateQueries({
+              queryKey: ["getColumns", projectId],
+            });
+          }
+        )
+        .subscribe();
+    }
+
+    return () => {
+      if (channel) {
+        supabase.removeChannel(channel);
+      }
+    };
+  }, [projectId, queryClient]);
+
   return useQuery({
     queryKey: ["getColumns", projectId],
     queryFn: async () => await getColumnsService(projectId!),
