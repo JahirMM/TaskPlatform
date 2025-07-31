1388cca53ca7b87729323c4feb73ddb49152629f
JahirMM <j.machuca912@gmail.com>
2025-07-31 14:26:12 -0400

    feat(useGetTasks): Add subscription to changes in the task list with Supabase Realtime

--- src/board/hook/useGetTasks.ts
+++ src/board/hook/useGetTasks.ts
@@ -1,7 +1,40 @@
 import { getTasksByProjectIdService } from "@/board/service/getTasksByProjectIdService";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { supabase } from "@/common/utils/supabase";
+import { useEffect } from "react";
 
 export const useGetTasksByProjectId = (projectId: string) => {
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    let channel: ReturnType<typeof supabase.channel> | undefined;
+
+    if (projectId) {
+      channel = supabase
+        .channel(`realtime:tasks:${projectId}`)
+        .on(
+          "postgres_changes",
+          {
+            event: "*",
+            schema: "public",
+            table: "tasks",
+          },
+          () => {
+            queryClient.invalidateQueries({
+              queryKey: ["getTasksByProjectId", projectId],
+            });
+          }
+        )
+        .subscribe();
+    }
+
+    return () => {
+      if (channel) {
+        supabase.removeChannel(channel);
+      }
+    };
+  }, [projectId, queryClient]);
+
   return useQuery({
     queryKey: ["getTasksByProjectId", projectId],
     queryFn: async () => await getTasksByProjectIdService(projectId),
