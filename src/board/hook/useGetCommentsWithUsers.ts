d68399e17cdc144c993834ba15720f941239ad8c
JahirMM <j.machuca912@gmail.com>
2025-07-31 17:47:52 -0400

    feat(useGetCommentsWithUser): Add subscription to changes in the comment list with Supabase Realtime

--- src/board/hook/useGetCommentsWithUsers.ts
+++ src/board/hook/useGetCommentsWithUsers.ts
@@ -1,7 +1,40 @@
 import { getCommentsWithUserService } from "@/board/service/getCommentsWithUserService";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { supabase } from "@/common/utils/supabase";
+import { useEffect } from "react";
 
 export const useGetCommentsWithUser = (taskId: string) => {
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    let channel: ReturnType<typeof supabase.channel> | undefined;
+
+    if (taskId) {
+      channel = supabase
+        .channel(`realtime:comment:${taskId}`)
+        .on(
+          "postgres_changes",
+          {
+            event: "*",
+            schema: "public",
+            table: "comments",
+          },
+          () => {
+            queryClient.invalidateQueries({
+              queryKey: ["getCommentsWithUsers", taskId],
+            });
+          }
+        )
+        .subscribe();
+    }
+
+    return () => {
+      if (channel) {
+        supabase.removeChannel(channel);
+      }
+    };
+  }, [taskId, queryClient]);
+
   return useQuery({
     queryKey: ["getCommentsWithUsers", taskId],
     queryFn: async () => await getCommentsWithUserService(taskId),
